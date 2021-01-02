a1019eb54bafcee2d6eae9280eab61949ea03a7e
cesarofuchi <ofuchi@utfpr.edu.br>
2021-01-02 11:15:51 -0300

    Update train.py
    edited version

--- optimizing_ml_pipeline_in_azure/train.py
+++ optimizing_ml_pipeline_in_azure/train.py
@@ -12,18 +12,12 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+#"https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+ds = Dataset.Tabular.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
-
+#should be defined before use
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -49,6 +43,18 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+
+### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.80,test_size=0.20, random_state=1)
+
+run = Run.get_context()
+
     
 
 def main():
